3749472227491182b9f3c0dccba5e8b271838e19
smv5047 <smv5047@gmail.com>
2019-10-06 16:00:10 -0400

    creted follower card component

---
+++ github-user-card/src/components/FollowerCard.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+
+class FollowerCard extends React.Component {
+ 
+
+
+    
+
+render(){
+    return (
+        <>
+            
+                <div>
+                    
+                    <img src={this.props.user.image} alt={this.props.user.name}/>
+                </div>
+                <div>
+                    <h1>{this.props.user.name}</h1>
+                    <h2>{this.props.user.location}</h2>
+                    <h1>Followers:{this.props.user.follower_count}</h1>
+                </div>
+            
+            
+        </>
+    );
+    }
+}
+
+export default FollowerCard
